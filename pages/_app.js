1801b722726a0200e151315fa5c955338b33a1c5
sdirosa <surai@objectiv.io>
2022-08-04 10:24:18 +0200

    Add LocaleContextPlugin demonstration

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,29 @@
+import { LocaleContextPlugin } from "@objectiv/plugin-locale-context";
 import { ObjectivProvider, ReactTracker } from "@objectiv/tracker-react";
+import { useRouter } from "next/router";
 
 if(process.env.NODE_ENV.startsWith('dev') && typeof window !== undefined) {
   require('@objectiv/developer-tools');
 }
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const { locale } = router;
+  const [languageCode, countryCode] = locale.split('-');
+
   const tracker = new ReactTracker({
     applicationId: 'nextjs-demo-app',
     transport: {
       transportName: 'ConsoleLogTransport',
       isUsable: () => true,
       handle: async (...args) => console.log(...args)
-    }
+    },
+    plugins:[
+      new LocaleContextPlugin({
+        languageFactoryFunction: () => languageCode,
+        countryFactoryFunction: () => countryCode,
+      })
+    ]
   })
 
   return (
